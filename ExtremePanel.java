17c963951a9e22d18361e86857f6cbeb8f23620d
Minhal Syed <MinhalSyed1@users.noreply.github.com>
2019-03-27 16:50:59 -0400

    Add files via upload

---
+++ ExtremePanel.java
@@ -0,0 +1,70 @@
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+
+// ExtremePanel creates the Input panels
+// Interacts with Update action class to Update the game
+public class ExtremePanel extends JPanel implements Observer {
+
+ private Grid[][] sudokuGrid; // Array of fields.
+ private JPanel[][] sudokuPanel; // Panels holding the fields.
+
+ //Creates grid and concatenates it with Panel
+ public ExtremePanel() {
+  super(new GridLayout(3, 3));
+
+  sudokuPanel = new JPanel[3][3];
+  for (int y = 0; y < 3; y++) {
+   for (int x = 0; x < 3; x++) {
+    sudokuPanel[y][x] = new JPanel(new GridLayout(3, 3));
+    sudokuPanel[y][x].setBorder(BorderFactory.createLineBorder(Color.GRAY));
+    add(sudokuPanel[y][x]);
+   }
+  }
+
+  sudokuGrid = new Grid[9][9];
+  for (int y = 0; y < 9; y++) {
+   for (int x = 0; x < 9; x++) {
+    sudokuGrid[y][x] = new Grid(x, y);
+    sudokuPanel[y / 3][x / 3].add(sudokuGrid[y][x]);
+   }
+  }
+ }
+
+
+
+ //Creates game hides Number Candidates to create Game
+ public void setGame(Game game) {
+  for (int y = 0; y < 9; y++) {
+   for (int x = 0; x < 9; x++) {
+    sudokuGrid[y][x].setBackground(Color.WHITE);
+    sudokuGrid[y][x].setInput(game.getNumber(x, y), false);
+   }
+  }
+ }
+
+
+
+//input Listener connects extreme panel to extreme controller inputs are Controled by the Controller
+ public void setExtremeController(ExtremeController extremeController) {
+  for (int y = 0; y < 3; y++) {
+   for (int x = 0; x < 3; x++)
+    sudokuPanel[y][x].addMouseListener(extremeController);
+  }
+ }
+ 
+ //Update notification
+ public void update(Observable o, Object arg) {
+  switch ((UpdateAction) arg) {
+   case NEW_GAME:
+    setGame((Game) o);
+    break;
+
+  }
+ }
+ 
+}
---
+++ InputsPanel.java
@@ -0,0 +1,84 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+
+
+
+public class InputsPanel extends JPanel implements Observer {
+	
+//Buttons To be Representend on the Inputs panel	
+
+ ButtonGroup groupNum; 
+ JToggleButton[] numberBTN; 
+ JButton newBTN;
+ JButton exitBTN; 
+
+//Update Notification to UpdateAction
+ public void update(Observable o, Object arg) {
+  switch ((UpdateAction) arg) {
+   case NEW_GAME:
+    break;
+  }
+ }
+ // Sets controller to Control inputs done by the user calls Action Controller class
+ public void setController(ActionController actionController) {
+  newBTN.addActionListener(actionController);
+  exitBTN.addActionListener(actionController);
+
+  for (int i = 0; i < 9; i++)
+   numberBTN[i].addActionListener(actionController);
+ }
+
+
+
+ public InputsPanel() {
+  super(new BorderLayout());
+  
+  groupNum = new ButtonGroup();
+  numberBTN = new JToggleButton[9];
+  
+  //Creates Panels and buttons of the game 
+  
+  JPanel gameAlign = new JPanel();
+  gameAlign.setLayout(new BoxLayout(gameAlign, BoxLayout.PAGE_AXIS));
+  add(gameAlign, BorderLayout.NORTH);
+  JPanel gameOptions = new JPanel(new FlowLayout(FlowLayout.LEADING));
+  gameOptions.setBorder(BorderFactory.createTitledBorder(" Menu "));
+  gameAlign.add(gameOptions);
+  exitBTN = new JButton("Close");
+  exitBTN.setFocusable(false);
+  gameOptions.add(exitBTN);
+  newBTN = new JButton("New");
+  newBTN.setFocusable(false);
+  gameOptions.add(newBTN);
+
+  JPanel gameNumbers = new JPanel();
+  gameNumbers.setLayout(new BoxLayout(gameNumbers, BoxLayout.PAGE_AXIS));
+  gameNumbers.setBorder(BorderFactory.createTitledBorder(" Numbers: "));
+  gameAlign.add(gameNumbers);
+
+
+  JPanel gameNumbers2 = new JPanel(new FlowLayout(FlowLayout.LEADING));
+  gameNumbers.add(gameNumbers2);
+
+
+  for (int i = 0; i < 9; i++) {
+   numberBTN[i] = new JToggleButton("" + (i + 1));
+   numberBTN[i].setPreferredSize(new Dimension(40, 40));
+   numberBTN[i].setFocusable(false);
+   groupNum.add(numberBTN[i]);
+   gameNumbers2.add(numberBTN[i]);
+  }
+ }
+
+
+}
