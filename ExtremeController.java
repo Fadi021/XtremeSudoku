c9d7c2432a791dccdf9309cbc63cd0e81d5881f8
Minhal Syed <MinhalSyed1@users.noreply.github.com>
2019-03-27 23:12:31 -0400

    Update ExtremeController.java

--- ExtremeController.java
+++ ExtremeController.java
@@ -5,7 +5,6 @@ import javax.swing.JPanel;
 
 
 // Controls all inputs made by the user in Extreme panel
-
 public class ExtremeController implements MouseListener {
  private ExtremePanel extremePanel; 
  private Game game; 
@@ -17,15 +16,12 @@ public class ExtremeController implements MouseListener {
  public void mouseReleased(MouseEvent e) {}
  
  // Sets game panel to control(extremePanel) and conects it to the game
- 
- 
  public ExtremeController(ExtremePanel extremePanel, Game game) {
   this.extremePanel = extremePanel;
   this.game = game;
  }
 
 //When user tries to input numbers, this functions is called and updates the game based off the action
- 
  public void mousePressed(MouseEvent e) {
   JPanel panel = (JPanel) e.getSource();
   Component component = panel.getComponentAt(e.getPoint());
@@ -35,7 +31,6 @@ public class ExtremeController implements MouseListener {
    int y = grid.getYPos();
 
 //Controls inputs made by the User, evaluates them and Updates candiates in Class UpdateAction 
-   
    if (e.getButton() == MouseEvent.BUTTON1) {
     int number = game.getInputNumber(); 
     grid.setInput(number, true);
