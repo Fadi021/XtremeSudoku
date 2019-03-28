4559734526371f9f2e99f8d7e04a7efba4347349
Fadi021 <48961086+Fadi021@users.noreply.github.com>
2019-03-27 23:18:44 -0400

    Update ActionController.java

--- ActionController.java
+++ ActionController.java
@@ -1,3 +1,4 @@
+//import libraries needed to perform various functions
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,13 +15,11 @@ public class ActionController implements ActionListener {
 // Performs the action inputed by the user
 // Sends an Update to the Game class based on the input of the User
  public void actionPerformed(ActionEvent e) {
-  // If user selects new, it creates a game
   if (e.getActionCommand().equals("New"))
    game.createGame();
-  // else if user selects close, it exits the game
   else if (e.getActionCommand().equals("Close"))
    System.exit(0);
-  // last option
+
   else
    game.setInputNumber(Integer.parseInt(e.getActionCommand()));
  }
